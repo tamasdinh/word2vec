18cf4520317a8eb104c0da06d55dcf8459e1f0ed
tamasdinh <tamas.dinh@gmail.com>
2019-05-11 20:12:07 +0200

    feat: updated script and refactored for command line run

--- Word2Vec.py
+++ Word2Vec.py
@@ -7,21 +7,17 @@ from torch import nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
 from sklearn.manifold import TSNE
-
-#%%
-with open(os.getcwd() + '/data/text8') as f:
-    text = f.read()
-print(text[:100])
+from datetime import datetime
 
 
 #%%
 def text_preprocess(text, occ_limit=5):
-    '''
+    """
     Light preprocessing of text for NLP purposes (punctuation encoding, frequency filtering)
     :param text: simple text format variable
     :param occ_limit: limit for word frequency (all words are kept that have a frequency higher than the limit)
     :return: list - cleaned text split by space
-    '''
+    """
     punct_dict = {
         '.': ' <PERIOD> ',
         ',': ' <COMMA> ',
@@ -46,16 +42,6 @@ def text_preprocess(text, occ_limit=5):
 
 
 #%%
-words = text_preprocess(text)
-print(words[:100])
-
-#%%
-# Take some stats on the text
-print('Total number of words in text: {}'.format(len(words)))
-print('Number of unique words in text: {}'.format(len(set(words))))
-
-
-#%%
 def create_lookup_tables(word_list):
     """
     Creates 2 dictionaries for vocabulary encoding: int2word and word2int
@@ -68,23 +54,6 @@ def create_lookup_tables(word_list):
     word2int = {word: index for index, word in int2word.items()}
 
     return int2word, word2int
-
-
-#%%
-int2word, word2int = create_lookup_tables(words)
-int_words = [word2int[word] for word in words]
-print(int_words[:30])
-
-#%%
-threshold = 1e-5
-word_counts = Counter(int_words)
-total_cnts = len(int_words)
-
-freqs = {word: count / total_cnts for word, count in word_counts.items()}
-del_probs = {word: 1 - np.sqrt(threshold / freqs[word]) for word in word_counts}
-
-train_words = [word for word in int_words if np.random.random() < 1 - del_probs[word]]
-print(train_words[:100])
 
 
 #%%
@@ -124,21 +93,11 @@ def get_batches(words, batch_size, window_size=5):
         batch = words[idx:idx+batch_size]
         for ii in range(len(batch)):
             batch_x = batch[ii]
-            batch_y = get_context(batch, ii, window_size=5)
+            batch_y = get_context(batch, ii, window_size)
             y.extend(batch_y)
             x.extend([batch_x]*len(batch_y))
 
         yield x, y
-
-
-#%%
-# Testing the batching
-int_text = [i for i in range(20)]
-x, y = next(get_batches(int_text, batch_size=4))
-
-print(int_text)
-print('x\n', x)
-print('y\n', y)
 
 
 #%%
@@ -175,8 +134,8 @@ def cosine_similarity(embedding, valid_size=16, valid_window=100, device='cpu'):
     magnitudes = embed_vectors.pow(2).sum(dim=1).sqrt().unsqueeze(0)
 
     # sampling n words from ranges (0, window) and (1000, 1000 + window). Lower IDs are more frequent
-    valid_examples = np.array(np.random.sample(range(valid_window)), valid_size // 2)
-    valid_examples = np.append(valid_examples, np.random.sample(range(1000, 1000 + valid_window), valid_size // 2))
+    valid_examples = np.array(np.random.choice(range(valid_window), valid_size // 2))
+    valid_examples = np.append(valid_examples, np.random.choice(range(1000, 1000 + valid_window), valid_size // 2))
     valid_examples = torch.LongTensor(valid_examples).to(device)
 
     valid_vectors = embedding(valid_examples)
@@ -186,51 +145,130 @@ def cosine_similarity(embedding, valid_size=16, valid_window=100, device='cpu'):
 
 
 #%%
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print('Training on {}'.format('CUDA' if device == 'cuda' else 'CPU'))
+def train(model, train_words, int2word, epochs=5, learning_rate=0.003, print_every=500, device='cpu'):
+    """
+    Implements training cycles for SkipGram model
+    :param model: instantiated SkipGram model
+    :param train_words: training text
+    :param int2word: integer-word encoding dictionary
+    :param epochs: no. of epochs to run
+    :param learning_rate: learning rate for neural network
+    :param print_every: steps interval for printing results
+    :return: trained model
+    """
+    criterion = nn.NLLLoss()
+    optimizer = optim.Adam(model.parameters(), learning_rate)
+    steps = 0
 
-embedding_dim = 300
-learning_rate = 0.003
+    for e in range(epochs):
+        print('----------------\nRunning epoch #{}'.format(e + 1))
 
-model = SkipGram(len(word2int), embedding_dim).to(device)
-criterion = nn.NLLLoss()
-optimizer = optim.Adam(model.parameters(), learning_rate)
+        for inputs, targets in get_batches(train_words, 512):
+            steps += 1
+            inputs, targets = torch.LongTensor(inputs), torch.LongTensor(targets)
+            inputs, targets = inputs.to(device), targets.to(device)
 
-print_every = 500
-steps = 0
-epochs = 1
+            log_ps = model(inputs)
+            loss = criterion(log_ps, targets)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if steps % print_every == 0:
+                valid_examples, valid_similarities = cosine_similarity(model.embed, device=device)
+                _, closest_idxs = valid_similarities.topk(6)
+
+                valid_examples, closest_idxs = valid_examples.to('cpu'), closest_idxs.to('cpu')
+                for ii, valid_idx in enumerate(valid_examples):
+                    closest_words = [int2word[idx.item()] for idx in closest_idxs[ii][1:]]
+                    print(int2word[valid_idx.item()] + '|' + ', '.join(closest_words))
+                print('...')
+    print('\nMODEL TRAINING COMPLETED AT END OF EPOCH {}'.format(e + 1))
+
+    return model
+
 
 #%%
-for e in range(epochs):
+def tsne_viz(model, int2word, words_to_chart=600):
+    """
+    Implements the visualization of a selected number of words from the corpus with tSNE.
+    :param model: trained SkipGram model
+    :param int2word: integer-word encoding dictionary
+    :param words_to_chart: no. of words to chart
+    :return: tSNE chart of selected number of words
+    """
+    embeddings = model.embed.weight.to('cpu').data.numpy()
+    tsne = TSNE()
+    embed_tsne = tsne.fit_transform(embeddings[:words_to_chart, :])
 
-    for inputs, targets in get_batches(train_words, 512):
-        steps += 1
-        inputs, targets = torch.LongTensor(inputs), torch.LongTensor(targets)
-        inputs, targets = inputs.to(device), targets.to(device)
+    fig, ax = plt.subplots(figsize=(16, 16))
+    for idx in range(words_to_chart):
+        plt.scatter(*embed_tsne[idx, :], color='blue')
+        plt.annotate(int2word[idx], (embed_tsne[idx, 0], embed_tsne[idx, 1]), alpha=0.7)
+    plt.savefig('skipgram_tSNE_{}'.format(datetime.today().date(), int(datetime.today().timestamp())))
 
-        log_ps = model(inputs)
-        loss = criterion(log_ps, targets)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if steps % print_every == 0:
-            valid_examples, similarities = cosine_similarity(model.embed, device=device)
-            _, closest_idxs = valid_similarities.topk(6)
-
-            valid_examples, closest_idxs = valid_examples.to('cpu'), closest_idxs.to('cpu')
-            for ii, valid_idx in enumerate(valid_examples):
-                closest_words = [int2word[idx.item()] for idx in closest_idxs[ii][1:]]
-                print(int2word[valid_idx.item()] + '|' + ', '.join(closest_words))
-            print(...)
 
 #%%
-embeddings = model.embed.weight.to('cpu').data.numpy()
-words_to_chart = 600
-tsne = TSNE()
-embed_tsne = tsne.fit_transform(embeddings[:words_to_chart, :])
+def main():
 
-fig, ax = plt.subplots(figsize=(16, 16))
-for idx in range(words_to_chart):
-    plt.scatter(*embed_tsne[idx, :], color='blue')
-    plt.annotate(int2word[idx], (embed_tsne[idx, 0], embed_tsne[idx, 1]), alpha=0.7)
+    with open(os.getcwd() + '/data/text8') as f:
+        text = f.read()
+    print(text[:100], '\n')
+
+    words = text_preprocess(text)
+    print(words[:100])
+
+    # Take some stats on the text
+    print('\nTotal number of words in text: {}'.format(len(words)))
+    print('Number of unique words in text: {}'.format(len(set(words))))
+
+    int2word, word2int = create_lookup_tables(words)
+    int_words = [word2int[word] for word in words]
+    print('\n', int_words[:30])
+
+    threshold = 1e-5
+    word_counts = Counter(int_words)
+    total_cnts = len(int_words)
+
+    freqs = {word: count / total_cnts for word, count in word_counts.items()}
+    del_probs = {word: 1 - np.sqrt(threshold / freqs[word]) for word in word_counts}
+
+    train_words = [word for word in int_words if np.random.random() < 1 - del_probs[word]]
+    print('\n', train_words[:100])
+
+    # Testing the batching
+    int_text = [i for i in range(20)]
+    x, y = next(get_batches(int_text, batch_size=4))
+
+    print(int_text)
+    print('x\n', x)
+    print('y\n', y)
+
+    # Setting up training
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print('\nTraining on {}'.format('CUDA' if device == 'cuda' else 'CPU'))
+
+    # Instantiating model
+    embedding_dim = 300
+    model = SkipGram(len(word2int), embedding_dim).to(device)
+
+    # Training the model
+    model = train(model, train_words, int2word)
+
+    # Saving the model
+    model_name = 'skipgram-simple_{}_{}.net'.format(datetime.today().date(), int(datetime.today().timestamp()))
+
+    checkpoint = {'state_dict': model.state_dict(),
+                  'embedding': model.embed}
+
+    with open(f'./{model_name}', 'wb') as f:
+        torch.save(checkpoint, f)
+
+    # Visualizing with tSNE
+    tsne_viz(model, int2word)
+
+
+#%%
+if __name__ == '__main__':
+    main()
+
